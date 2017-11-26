0b41d8af6d3a89a4bd3158e4e8585ab92e0a344b
Zhou Ji <zhouji@Zhous-MacBook-Pro.local>
2017-11-26 00:13:03 -0500

    update db scripts

--- tools/add_submission_centers.sql
+++ tools/add_submission_centers.sql
@@ -1,30 +1,21 @@
 -- this script adds submission centers to the database
-SET @center_name = "Boston Children's Hospital";
-SELECT MAX(id)+1 into @id FROM dashboard_entity;
-INSERT INTO dashboard_entity (id, displayName) VALUES ( @id, @center_name );
-INSERT INTO submission_center (id) VALUES (@id);
+USE hip_signature;
 
-SET @center_name = 'Columbia University';
-SELECT MAX(id)+1 into @id FROM dashboard_entity;
-INSERT INTO dashboard_entity (id, displayName) VALUES ( @id, @center_name );
-INSERT INTO submission_center (id) VALUES (@id);
+DELIMITER //
+CREATE PROCEDURE addSubmissionCenter(IN center_name TEXT)
+BEGIN
+    SELECT MAX(id)+1 into @id FROM dashboard_entity;
+    INSERT INTO dashboard_entity (id, displayName) VALUES ( @id, center_name );
+    INSERT INTO submission_center (id) VALUES (@id);
+END//
+DELIMITER ;
 
-SET @center_name = 'Emory University';
-SELECT MAX(id)+1 into @id FROM dashboard_entity;
-INSERT INTO dashboard_entity (id, displayName) VALUES ( @id, @center_name );
-INSERT INTO submission_center (id) VALUES (@id);
-
-SET @center_name = 'Icahn School of Medicine at Mount Sinai';
-SELECT MAX(id)+1 into @id FROM dashboard_entity;
-INSERT INTO dashboard_entity (id, displayName) VALUES ( @id, @center_name );
-INSERT INTO submission_center (id) VALUES (@id);
-
-SET @center_name = 'La Jolla Institute for Allergy and Immunology';
-SELECT MAX(id)+1 into @id FROM dashboard_entity;
-INSERT INTO dashboard_entity (id, displayName) VALUES ( @id, @center_name );
-INSERT INTO submission_center (id) VALUES (@id);
-
-SET @center_name = 'Yale University';
-SELECT MAX(id)+1 into @id FROM dashboard_entity;
-INSERT INTO dashboard_entity (id, displayName) VALUES ( @id, @center_name );
-INSERT INTO submission_center (id) VALUES (@id);
+CALL addSubmissionCenter("Boston Children's Hospital");
+CALL addSubmissionCenter("Center for Infectious Disease Research");
+CALL addSubmissionCenter("Columbia University");
+CALL addSubmissionCenter("Drexel University");
+CALL addSubmissionCenter('Emory University');
+CALL addSubmissionCenter('Icahn School of Medicine at Mount Sinai');
+CALL addSubmissionCenter('La Jolla Institute for Allergy and Immunology');
+CALL addSubmissionCenter('University of California Los Angeles');
+CALL addSubmissionCenter('Yale University');
---
+++ tools/reset_database.sql
@@ -0,0 +1,4 @@
+-- this script resets the database before re-loading everything
+
+DROP DATABASE IF EXISTS hip_signature;
+CREATE DATABASE hip_signature;
