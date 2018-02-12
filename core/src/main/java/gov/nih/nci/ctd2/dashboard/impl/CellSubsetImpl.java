fd0582cbb2f6344bf5785e9c07755f4f6cde0b96
Zhou Ji <zj2122@columbia.edu>
2018-02-12 17:17:20 -0500

    add new Subject CellSubset. issue #7

---
+++ core/src/main/java/gov/nih/nci/ctd2/dashboard/impl/CellSubsetImpl.java
@@ -0,0 +1,84 @@
+package gov.nih.nci.ctd2.dashboard.impl;
+
+import gov.nih.nci.ctd2.dashboard.model.CellSubset;
+import gov.nih.nci.ctd2.dashboard.model.Synonym;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+import org.hibernate.annotations.Proxy;
+import org.hibernate.search.annotations.Indexed;
+
+import javax.persistence.*;
+
+@Entity
+@Proxy(proxyClass = CellSubset.class)
+@Table(name = "cellSubset")
+@Indexed
+public class CellSubsetImpl extends SubjectImpl implements CellSubset {
+
+	private static final long serialVersionUID = -4774943578771816570L;
+
+	private String cellOntologyId;
+    private String definition;
+	private String comment;
+	private Set<Synonym> exactSynonyms = new LinkedHashSet<Synonym>();
+	private Set<Synonym> related = new LinkedHashSet<Synonym>();
+
+	@Override
+	public String getCellOntologyId() {
+		return cellOntologyId;
+	}
+
+	@Override
+	public void setCellOntologyId(String cellOntologyId) {
+		this.cellOntologyId = cellOntologyId;
+	}
+
+	@Override
+	public String getDefinition() {
+		return definition;
+	}
+
+	@Override
+	public void setDefinition(String definition) {
+		this.definition = definition;
+	}
+
+	@Override
+	public String getComment() {
+		return comment;
+	}
+
+	@Override
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	@LazyCollection(LazyCollectionOption.FALSE)
+    @OneToMany(targetEntity = SynonymImpl.class, cascade = CascadeType.ALL)
+	@JoinTable(name = "exact_synonym_map")
+	@Override
+	public Set<Synonym> getExactSynonyms() {
+		return exactSynonyms;
+	}
+
+	@Override
+	public void setExactSynonyms(Set<Synonym> exactSynonyms) {
+		this.exactSynonyms = exactSynonyms;
+	}
+
+	@LazyCollection(LazyCollectionOption.FALSE)
+    @OneToMany(targetEntity = SynonymImpl.class, cascade = CascadeType.ALL)
+	@JoinTable(name = "related_synonym_map")
+	@Override
+	public Set<Synonym> getRelated() {
+		return related;
+	}
+
+	@Override
+	public void setRelated(Set<Synonym> related) {
+		this.related = related;
+	}
+
+}
---
+++ core/src/main/java/gov/nih/nci/ctd2/dashboard/model/CellSubset.java
@@ -0,0 +1,17 @@
+package gov.nih.nci.ctd2.dashboard.model;
+
+import java.util.Set;
+
+public interface CellSubset extends Subject {
+    public String getCellOntologyId();
+    public void setCellOntologyId(String cellOntologyId);
+    public String getDefinition();
+    public void setDefinition(String definition);
+    public String getComment();
+    public void setComment(String comment);
+    /* Subject.synonym is interpred as "Broad synonyms " for Cell Subset. */
+    public Set<Synonym> getExactSynonyms();
+    public void setExactSynonyms(Set<Synonym> exactSynonyms);
+    public Set<Synonym> getRelated();
+    public void setRelated(Set<Synonym> related);
+}
