f07584f238f659541fae56906ad66fcc47b13939
Zhou Ji <zj2122@columbia.edu>
2018-11-14 12:47:13 -0500

    enable assertion

--- load_submission_data.bat
+++ load_submission_data.bat
@@ -3,9 +3,9 @@ rem load the submission data. this should be done after loading the background d
 IF NOT DEFINED HIPC_DATA_HOME SET HIPC_DATA_HOME=C:\data_collection\hipc_data
 
 echo start loading controlled vocabulary %time%
-java -jar admin\target\dashboard-admin.jar -cv
+java -ea -jar admin\target\dashboard-admin.jar -cv
 echo start loading submission data %time%
-java -jar admin\target\dashboard-admin.jar -o
+java -ea -jar admin\target\dashboard-admin.jar -o
 
 echo start indexing %time%
 java -jar admin\target\dashboard-admin.jar -i
