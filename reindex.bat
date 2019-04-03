ff8b9d205763acfd62bcb1788dee11a19517f2e4
Zhou Ji <zj2122@columbia.edu>
2019-04-03 16:12:43 -0400

    minor improvement in testing script

--- reindex.bat
+++ reindex.bat
@@ -1,9 +1,12 @@
 rem rebuild index files, which is the most time-consuming task
 
+IF NOT DEFINED HIPC_DATA_HOME SET HIPC_DATA_HOME=C:\data_collection\hipc_data
+rmdir /s C:\index-base\hipc-signatures-index\gov.nih.nci.ctd2.dashboard.impl.DashboardEntityImpl
+
 set start=%time%
 echo start time %start%
 
-java -Xmx1024m -jar admin\target\dashboard-admin.jar -i
+java -jar admin\target\dashboard-admin.jar -i
 
 set end=%time%
 echo end time %end%
