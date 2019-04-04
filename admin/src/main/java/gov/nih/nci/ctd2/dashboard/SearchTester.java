862e6c22b3e15cd5677b5a03c5d361c8d232bc01
Zhou Ji <zj2122@columbia.edu>
2019-04-04 17:56:33 -0400

    add a simple tool to test searching

---
+++ admin/src/main/java/gov/nih/nci/ctd2/dashboard/SearchTester.java
@@ -0,0 +1,51 @@
+package gov.nih.nci.ctd2.dashboard;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.transaction.annotation.Transactional;
+
+import gov.nih.nci.ctd2.dashboard.dao.DashboardDao;
+import gov.nih.nci.ctd2.dashboard.util.DashboardEntityWithCounts;
+
+/* 
+    A simple tool to test searching functionality outside the web application. 
+    Usage: java -cp admin\target\dashboard-admin.jar gov.nih.nci.ctd2.dashboard.SearchTester brisbane
+    Note: brisbane is an example term to search for.
+*/
+public class SearchTester {
+    private static final Log log = LogFactory.getLog(SearchTester.class);
+
+    private static final ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext(
+            "classpath*:META-INF/spring/applicationContext.xml", // This is for DAO/Dashboard Model
+
+            /* the following must be included just to make it compilable */
+            "classpath*:META-INF/spring/cellLineDataApplicationContext.xml",
+            "classpath*:META-INF/spring/compoundDataApplicationContext.xml",
+            "classpath*:META-INF/spring/TRCshRNADataApplicationContext.xml",
+            "classpath*:META-INF/spring/tissueSampleDataApplicationContext.xml",
+            "classpath*:META-INF/spring/controlledVocabularyApplicationContext.xml",
+            "classpath*:META-INF/spring/observationDataApplicationContext.xml");
+
+    @Transactional
+    public static void main(String[] args) {
+
+        DashboardDao dao = (DashboardDao) appContext.getBean("dashboardDao");
+        log.debug("starting time " + new Date());
+        ArrayList<DashboardEntityWithCounts> list = dao.search(args[0]);
+        log.debug("finished time " + new Date());
+        System.out.println("returned size is " + list.size());
+        int c = 0;
+        for (DashboardEntityWithCounts x : list) {
+            c++;
+            if (c > 10)
+                break;
+            System.out.println(x.getDashboardEntity().getId() + "," + x.getDashboardEntity().getDisplayName());
+        }
+        log.debug("done with testing " + new Date());
+    }
+
+}
