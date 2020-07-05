5e07ebfa13643223cc33140632e997e97493fc94
Zhou Ji <zj2122@columbia.edu>
2020-07-05 16:20:06 -0400

    fix an incorrect size limit

--- web/src/main/java/gov/nih/nci/ctd2/dashboard/controller/ListController.java
+++ web/src/main/java/gov/nih/nci/ctd2/dashboard/controller/ListController.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,24 +27,12 @@ public class ListController {
     @Autowired
     private WebServiceUtil webServiceUtil;
 
-    @Autowired
-    @Qualifier("maxNumberOfEntities")
-    private Integer maxNumberOfEntities = 100;
-
     public WebServiceUtil getWebServiceUtil() {
         return webServiceUtil;
     }
 
     public void setWebServiceUtil(WebServiceUtil webServiceUtil) {
         this.webServiceUtil = webServiceUtil;
-    }
-
-    public Integer getMaxNumberOfEntities() {
-        return maxNumberOfEntities;
-    }
-
-    public void setMaxNumberOfEntities(Integer maxNumberOfEntities) {
-        this.maxNumberOfEntities = maxNumberOfEntities;
     }
 
     @Transactional
@@ -58,9 +45,6 @@ public class ListController {
         headers.add("Content-Type", "application/json; charset=utf-8");
 
         List<? extends DashboardEntity> entities = webServiceUtil.getDashboardEntities(type, filterBy);
-        if (!getAll && entities.size() > getMaxNumberOfEntities()) {
-            entities = entities.subList(0, getMaxNumberOfEntities());
-        }
 
         JSONSerializer jsonSerializer = new JSONSerializer().transform(new ImplTransformer(), Class.class)
                 .transform(new DateTransformer(), Date.class);
