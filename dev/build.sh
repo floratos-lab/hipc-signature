59f6f1cb51e0d113652762e52eb67f78ffd387a2
Zhou Ji <zj2122@columbia.edu>
2021-03-19 14:55:09 -0400

    new release version

--- dev/build.sh
+++ dev/build.sh
@@ -3,4 +3,4 @@ mvn clean package -Ddatabase.username=root -Ddatabase.password=$DB_PASSWORD\
  -Ddatabase.url="jdbc:mysql://127.0.0.1:3306/hipc?serverTimezone=America/New_York"\
  -Ddatabase.test.url="jdbc:mysql://127.0.0.1:3306/hipc_test?serverTimezone=America/New_York"\
  -Ddatabase.test.clean.skip=true\
- -Ddashboard.release.version=v1.0.2 #`date +%Y.%m.%d`
+ -Ddashboard.release.version=v1.1.0 #`date +%Y.%m.%d`
