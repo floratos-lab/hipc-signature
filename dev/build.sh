fdff7018318d13aca688163241c93dce5bed8d6a
Zhou Ji <zj2122@columbia.edu>
2021-07-27 22:28:15 -0400

    add webpack in build script

--- dev/build.sh
+++ dev/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -x
+npx webpack
 mvn clean package -Ddatabase.username=root -Ddatabase.password=$DB_PASSWORD\
  -Ddatabase.url="jdbc:mysql://127.0.0.1:3306/hipc?serverTimezone=America/New_York"\
  -Ddatabase.test.url="jdbc:mysql://127.0.0.1:3306/hipc_test?serverTimezone=America/New_York"\
