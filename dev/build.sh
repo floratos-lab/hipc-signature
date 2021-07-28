7d5072bfa95f467971b611e2e8aa7c8c502f8146
Zhou Ji <zj2122@columbia.edu>
2021-07-27 22:37:43 -0400

    add comment regarding skipping test

--- dev/build.sh
+++ dev/build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -x
 npx webpack
+
+# -DskipTests compiles the tests, but skips running them
+# -Dmaven.test.skip=true skips compiling the tests and does not run them
+# see http://maven.apache.org/surefire/maven-surefire-plugin/examples/skipping-tests.html
+
 mvn clean package -Ddatabase.username=root -Ddatabase.password=$DB_PASSWORD\
  -Ddatabase.url="jdbc:mysql://127.0.0.1:3306/hipc?serverTimezone=America/New_York"\
  -Ddatabase.test.url="jdbc:mysql://127.0.0.1:3306/hipc_test?serverTimezone=America/New_York"\
